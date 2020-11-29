4b1481cf4627a14650e732ba0d83b8dade5a7084
eren2137183 <rengwl927@protonmail.ch>
2020-11-29 14:34:21 +0100

    Deploy

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueGAPI from 'vue-gapi'
+
 
 Vue.config.productionTip = false
 
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 
  
 // Use the plugin and pass along the configuration
-Vue.use(VueGAPI, apiConfig)
+
 
 new Vue({
   render: h => h(App),
