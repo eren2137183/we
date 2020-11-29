987d076e9ade3658be348745eaee789d72c6a6c4
Nere <74774741+eren2137183@users.noreply.github.com>
2020-11-29 14:52:35 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueGAPI from 'vue-gapi'
+
 
 Vue.config.productionTip = false
 
 
 
 
- 
-// Use the plugin and pass along the configuration
-Vue.use(VueGAPI, apiConfig)
 
 new Vue({
   render: h => h(App),
